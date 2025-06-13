f1620af3267cbc3fbb5f9e2b39b784c672554ac7
TheScore1 <113776816+TheScore1@users.noreply.github.com>
2025-06-14 02:52:13 +0500

    Add files via upload

---
+++ create_db.py
@@ -0,0 +1,11 @@
+import asyncio
+from db import engine, Base
+
+# alembic revision --autogenerate -m "added Instruments"
+# alembic upgrade head
+
+async def init_models():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+asyncio.run(init_models())
